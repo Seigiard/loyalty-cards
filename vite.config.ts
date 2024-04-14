6f0a839b2b7c588f7efc2288bb775aa372df127b
Andrew Borisenko <seigiard@gmail.com>
2024-04-14 17:46:16 +0200

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,73 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const BASE_FOLDER = '/loyalty-cards/';
+
 export default defineConfig({
   base: '/loyalty-cards/',
-  plugins: [VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Loyalty Cards',
+        short_name: 'LoyaltyCards',
+        id: 'andrej.borisenko.loyalty-cards',
+        description: 'Store your discount and loyalty cards in your phone',
+        theme_color: '#ffffff',
+        display_override: ['fullscreen', 'minimal-ui'],
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon-maskable-192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icon-maskable-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icon-maskable-512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icon-maskable-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icon-192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icon-512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
+  ],
 });
