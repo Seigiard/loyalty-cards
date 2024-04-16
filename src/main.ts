1e274e7ee141f00dbceb99d080fafb1ac96370b3
Andrew Borisenko <seigiard@gmail.com>
2024-04-16 10:23:54 +0200

    fix build

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ component('#cards', cards);
 component('#card', card);
 
 const shadowBox = document.body;
-trackDeviceOrientationWithMouseFallback((baseX, baseY) => {
-  shadowBox.style.setProperty('--point-x', baseX);
-  shadowBox.style.setProperty('--point-y', baseY);
+trackDeviceOrientationWithMouseFallback((baseX: number, baseY: number) => {
+  shadowBox.style.setProperty('--point-x', baseX.toString());
+  shadowBox.style.setProperty('--point-y', baseY.toString());
 });
 
 function trackDeviceOrientationWithMouseFallback(
-  cb,
+  cb: (baseX: number, baseY: number) => void,
   { sensitivity } = { sensitivity: 15 }
 ) {
   const tCb = throttle(cb, 50);
@@ -38,27 +38,30 @@ function trackDeviceOrientationWithMouseFallback(
   });
 }
 
-function getProportion(base, x) {
-  return Math.round((base * 100) / x, 0);
+function getProportion(base: number, x: number) {
+  return Math.round((base * 100) / x);
 }
 
-function throttle(func, limit) {
-  let lastFunc;
-  let lastRan;
+function throttle<T>(func: T, limit: number) {
+  let lastFunc: NodeJS.Timeout;
+  let lastRan: number;
   return function () {
+    // @ts-ignore
     const context = this;
     const args = arguments;
     if (!lastRan) {
+      // @ts-ignore
       func.apply(context, args);
       lastRan = Date.now();
     } else {
       clearTimeout(lastFunc);
       lastFunc = setTimeout(function () {
         if (Date.now() - lastRan >= limit) {
+          // @ts-ignore
           func.apply(context, args);
           lastRan = Date.now();
         }
       }, limit - (Date.now() - lastRan));
     }
-  };
+  } as T;
 }
